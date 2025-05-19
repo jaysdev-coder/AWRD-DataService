c194904b623dec1a659dd540740e9dc11d066794
gleuzoi <guelzoi@outlook.com>
2025-05-19 14:36:22 -0700

    doc: base doc for query classes

--- Queries/IQueryT.cs
+++ Queries/IQueryT.cs
@@ -1,10 +1,34 @@
 ﻿namespace AWRD.Queries
+{ 
+/// <summary>
+/// Defines a generic interface for building and compiling queries.
+/// </summary>
+/// <typeparam name="T">The type of query being built.</typeparam>
+public interface IQueryT<T>
 {
-    public interface IQueryT<T>
-    {
-        public abstract T BuildBaseQuery();
-        public abstract T BuildCustomQuery(T query);
-        public abstract T GetComposedQuery();
-        public abstract string CompileQuery();
-    }
+    /// <summary>
+    /// Builds the base query.
+    /// </summary>
+    /// <returns>The base query.</returns>
+    public abstract T BuildBaseQuery();
+
+    /// <summary>
+    /// Builds a custom query based on the provided query.
+    /// </summary>
+    /// <param name="query">The query to customize.</param>
+    /// <returns>The custom query.</returns>
+    public abstract T BuildCustomQuery(T query);
+
+    /// <summary>
+    /// Gets the composed query.
+    /// </summary>
+    /// <returns>The composed query.</returns>
+    public abstract T GetComposedQuery();
+
+    /// <summary>
+    /// Compiles the query into a string representation.
+    /// </summary>
+    /// <returns>The compiled query string.</returns>
+    public abstract string CompileQuery();
+}
 }
--- Queries/ISqlQueryT.cs
+++ Queries/ISqlQueryT.cs
@@ -3,12 +3,24 @@ using SqlKata;
 
 namespace AWRD.Queries
 {
+    /// <summary>
+    /// Represents a generic SQL query class for a specific entity type.
+    /// </summary>
+    /// <typeparam name="T">The type of entity being queried against the table, which must implement ITableModel.</typeparam>
     public abstract class ISqlQueryT<T> : ISqlQuery where T : ITableModel
     {
+        /// <summary>
+        /// Gets the name of the entity being queried against the table.
+        /// </summary>
         public abstract string EntityName { get; }
 
+        /// <summary>
+        /// Builds the base query for the entity.
+        /// </summary>
+        /// <returns>A Query object representing the base query.</returns>
         public override Query BuildBaseQuery()
         {
+            // Create a new Query object and specify the entity (table) to query
             return new Query().From(EntityName);
         }
     }
