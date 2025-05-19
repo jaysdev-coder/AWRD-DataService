81adcd2ca2ed17109d8d89ebe6d9ebf886e5af2f
gleuzoi <guelzoi@outlook.com>
2025-05-19 12:21:50 -0700

    feat: add base implmentation for bulding base query method on abstract sql query T class
    This commit introduces the `SqlKata` namespace to `ISqlQueryT.cs` and implements the `BuildBaseQuery` method. The new method constructs a `Query` object that utilizes the `EntityName` property, enhancing the abstract class's ability to build base queries effectively.

--- Queries/ISqlQueryT.cs
+++ Queries/ISqlQueryT.cs
@@ -1,9 +1,15 @@
 ﻿using AWRD.DataService.Model;
+using SqlKata;
 
 namespace AWRD.Queries
 {
     public abstract class ISqlQueryT<T> : ISqlQuery where T : ITableModel
     {
         public abstract string EntityName { get; }
+
+        public override Query BuildBaseQuery()
+        {
+            return new Query().From(EntityName);
+        }
     }
 }
