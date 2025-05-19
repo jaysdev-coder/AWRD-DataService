0e4be833d228ef2d68980a44bcb505369a304d97
gleuzoi <guelzoi@outlook.com>
2025-05-19 00:12:08 -0700

    feat: sql query, sql query t type interface

--- Queries/ISqlQuery.cs
+++ Queries/ISqlQuery.cs
@@ -1,7 +1,26 @@
-﻿namespace AWRD.Queries
+﻿using SqlKata;
+using SqlKata.Compilers;
+
+namespace AWRD.Queries
 {
-    internal interface ISqlQuery : IQuery
+    public abstract class ISqlQuery : IQueryT<Query>
     {
-        abstract string TableName { get; init; }
+        public abstract Query BuildBaseQuery();
+
+        public abstract Query BuildCustomQuery(Query query);
+
+        public virtual Query GetComposedQuery()
+        {
+            return BuildCustomQuery(BuildBaseQuery());
+        }
+
+        public string CompileQuery()
+        {
+            var compiler = new SqlServerCompiler();
+
+            var query = compiler.Compile(GetComposedQuery());
+
+            return query.ToString();
+        }
     }
 }
---
+++ Queries/ISqlQueryT.cs
@@ -0,0 +1,9 @@
+﻿using AWRD.DataService.Model;
+
+namespace AWRD.Queries
+{
+    public abstract class ISqlQueryT<T> : ISqlQuery where T : ITableModel
+    {
+        public abstract string EntityName { get; }
+    }
+}
