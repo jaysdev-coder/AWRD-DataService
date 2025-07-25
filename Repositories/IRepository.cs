2ecd16a9edf71ec3547897680a618fb65a33a149
gleuzoi <guelzoi@outlook.com>
2025-07-24 21:54:07 -0700

    feat: introduce IRepository for data action

---
+++ Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using AWRD.QueryService.Model;
+
+namespace AWRD.Repositories
+{
+    internal interface IRepository<T> where T : ITableModel
+    {
+        Task Insert(T model);
+        Task Update(T model);
+        Task Delete(T model);
+    }
+}
