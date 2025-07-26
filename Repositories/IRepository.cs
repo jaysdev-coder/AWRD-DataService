9cad98ca5befec58cef80cb52624cc63a7953179
gleuzoi <guelzoi@outlook.com>
2025-07-25 22:00:28 -0700

    feat: add more methods for Repository interface class

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -4,6 +4,8 @@ namespace AWRD.Repositories
 {
     internal interface IRepository<T> where T : ITableModel
     {
+        Task GetById(int id);
+        Task<IEnumerable<T>> GetAll();
         Task Insert(T model);
         Task Update(T model);
         Task Delete(T model);
